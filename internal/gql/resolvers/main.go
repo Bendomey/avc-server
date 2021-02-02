7a6d9bf9d212bc2a107580bd5473b2985f4c6447
Benjamin Domey <domeybenjamin1@gmail.com>
2021-02-02 04:37:57 +0000

    called all resolvers here

--- internal/gql/resolvers/main.go
+++ internal/gql/resolvers/main.go
@@ -1,22 +1,38 @@
 package resolvers
 
 import (
-	"github.com/Bendomey/avc-server/internal/gql/schemas"
+	"github.com/Bendomey/avc-server/pkg/utils"
 	"github.com/graphql-go/graphql"
 )
+
+// ResolverLoader defines the reolver object
+type ResolverLoader struct {
+	Query    map[string]*graphql.Field
+	Mutation map[string]*graphql.Field
+}
+
+var queriesGathering = []map[string]*graphql.Field{
+	country.Query,
+	admin.Query,
+}
+
+var mutationsGathering = []map[string]*graphql.Field{
+	country.Mutation,
+	admin.Mutation,
+}
 
 // QueryType is the main querytype implementation
 var QueryType = graphql.NewObject(
 	graphql.ObjectConfig{
-		Name: "Query",
-		Fields: graphql.Fields{
-			"countries": &graphql.Field{
-				Type:        graphql.NewList(schemas.CountryType),
-				Description: "Get country list",
-				Resolve: func(params graphql.ResolveParams) (interface{}, error) {
-					return nil, nil
-				},
-			},
-		},
+		Name:   "Query",
+		Fields: graphql.Fields(utils.GetReolvers(queriesGathering)),
+	},
+)
+
+// MutationType is the main querytype implementation
+var MutationType = graphql.NewObject(
+	graphql.ObjectConfig{
+		Name:   "Mutation",
+		Fields: graphql.Fields(utils.GetReolvers(mutationsGathering)),
 	},
 )
