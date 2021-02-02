940eaec17aa19259a78de8bdf6e35c2ee7c632bb
Benjamin Domey <domeybenjamin1@gmail.com>
2021-02-02 02:40:04 +0000

    added super admin seed

--- internal/orm/migration/jobs/seed_super_admin.go
+++ internal/orm/migration/jobs/seed_super_admin.go
@@ -1,32 +1,32 @@
 package jobs
 
 import (
+	"github.com/Bendomey/avc-server/internal/orm/models"
 	"github.com/Bendomey/avc-server/pkg/utils"
 	"github.com/go-gormigrate/gormigrate/v2"
+	"gorm.io/gorm"
 )
 
 var (
-	fullname = utils.MustGet("ADMIN_NAME")
-	email    = utils.MustGet("ADMIN_EMAIL")
-	password = utils.MustGet("ADMIN_PASSWORD")
-	phone    = utils.MustGet("ADMIN_PHONE")
-	role     = utils.MustGet("ADMIN_ROLE")
-	// superAdmin *models.Admin = &models.Admin{
-	// 	FullName: fullname,
-	// 	Email:    email,
-	// 	Password: password,
-	// 	Role:     role,
-	// 	Phone:    &phone,
-	// }
+	fullname   = utils.MustGet("ADMIN_NAME")
+	email      = utils.MustGet("ADMIN_EMAIL")
+	password   = utils.MustGet("ADMIN_PASSWORD")
+	phone      = utils.MustGet("ADMIN_PHONE")
+	superAdmin = &models.Admin{
+		FullName: fullname,
+		Email:    email,
+		Password: password,
+		Phone:    &phone,
+	}
 )
 
 // SeedSuperAdmin inserts the super user
 var SeedSuperAdmin *gormigrate.Migration = &gormigrate.Migration{
 	ID: "SEED_SUPER_ADMIN",
-	// Migrate: func(db *gorm.DB) error {
-	// 	// return db.Create(&superAdmin).Error
-	// },
-	// Rollback: func(db *gorm.DB) error {
-	// 	return db.Delete(&superAdmin).Error
-	// },
+	Migrate: func(db *gorm.DB) error {
+		return db.Create(&superAdmin).Error
+	},
+	Rollback: func(db *gorm.DB) error {
+		return db.Delete(&superAdmin).Error
+	},
 }
