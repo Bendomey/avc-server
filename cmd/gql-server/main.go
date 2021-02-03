12663aac1cf09a852901fa849fefc5f6f41acb4d
Benjamin Domey <domeybenjamin1@gmail.com>
2021-02-03 13:54:15 +0000

    started services here

--- cmd/gql-server/main.go
+++ cmd/gql-server/main.go
@@ -4,19 +4,23 @@ import (
 	log "github.com/Bendomey/avc-server/internal/logger"
 	"github.com/Bendomey/avc-server/internal/orm"
 	"github.com/Bendomey/avc-server/internal/redis"
+	"github.com/Bendomey/avc-server/internal/services"
 	"github.com/Bendomey/avc-server/pkg/server"
 )
 
 func main() {
 	//connects to redis
-	redis.Factory()
+	rdb := redis.Factory()
 
 	// creates a new ORM instance to send it to our server
-	_, err := orm.Factory()
+	orm, err := orm.Factory()
 	if err != nil {
 		log.Panic("[ORM ERR] :: ", err)
 	}
 
+	//start services here
+	services := services.Factory(orm, rdb)
+
 	// server invoked here
-	server.Run()
+	server.Run(services)
 }
