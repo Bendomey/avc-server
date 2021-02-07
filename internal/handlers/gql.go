4d487ff35f01f7bb24177286dbc5d5b2fdb5b893
Benjamin Domey <domeybenjamin1@gmail.com>
2021-02-07 22:34:34 +0000

    fixed cors error

--- internal/handlers/gql.go
+++ internal/handlers/gql.go
@@ -37,7 +37,14 @@ func CreateGQLServer(r []*graphql.Object) *handler.Handler {
 //PlaygroundHanlder registers a route for plaground access
 func PlaygroundHanlder(handler *handler.Handler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		setupResponse(&w, r)
 		ctx := context.WithValue(r.Context(), utils.GetPrincipalID(), r.Header.Get("Authorization"))
 		handler.ContextHandler(ctx, w, r)
 	}
+}
+
+func setupResponse(w *http.ResponseWriter, req *http.Request) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 }
