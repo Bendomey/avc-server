b1bda64ea4241d2ec3f81090a14f358b57b3f1ca
Benjamin Domey <domeybenjamin1@gmail.com>
2021-02-27 22:05:35 +0000

    updated faq schema

--- internal/gql/schemas/faq.go
+++ internal/gql/schemas/faq.go
@@ -17,13 +17,40 @@ var FaqType = graphql.NewObject(
 				Type: graphql.NewNonNull(graphql.String),
 			},
 			"createdBy": &graphql.Field{
-				Type: AdminType,
+				Type: graphql.NewNonNull(AdminType),
 			},
 			"createdAt": &graphql.Field{
 				Type: graphql.DateTime,
 			},
 			"updatedAt": &graphql.Field{
 				Type: graphql.NewNonNull(graphql.DateTime),
+			},
+		},
+	},
+)
+
+//FilterFaqsType  for filtering faqs type
+var FilterFaqsType = graphql.NewInputObject(
+	graphql.InputObjectConfig{
+		Name: "GetFAQsFilter",
+		Fields: graphql.InputObjectConfigFieldMap{
+			"question": &graphql.InputObjectFieldConfig{
+				Type: graphql.String,
+			},
+			"answer": &graphql.InputObjectFieldConfig{
+				Type: graphql.String,
+			},
+			"search": &graphql.InputObjectFieldConfig{
+				Type: SearchType,
+			},
+			"order": &graphql.InputObjectFieldConfig{
+				Type: enumTypeForOrder,
+			},
+			"orderBy": &graphql.InputObjectFieldConfig{
+				Type: graphql.String,
+			},
+			"dateRange": &graphql.InputObjectFieldConfig{
+				Type: DateRangeType,
 			},
 		},
 	},
