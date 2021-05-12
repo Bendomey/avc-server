6a956d292f1b914d5573d43abc4f9152dff0c583
Benjamin Domey <domeybenjamin1@gmail.com>
2021-05-12 10:13:31 +0000

    package service model designed

--- internal/orm/models/packageService.go
+++ internal/orm/models/packageService.go
@@ -9,11 +9,13 @@ const (
 
 type PackageService struct {
 	BaseModelSoftDelete
-	ServiceID string `gorm:"not null;"`
-	Service   Service
-	PackageID string `gorm:"not null;"`
-	Package   Package
-	Type      PackageServiceType
-	Quantity  *int
-	isActive  *bool
+	ServiceID   string `gorm:"not null;"`
+	Service     Service
+	PackageID   string `gorm:"not null;"`
+	Package     Package
+	Type        PackageServiceType
+	Quantity    *int
+	IsActive    *bool
+	CreatedByID string
+	CreatedBy   Admin
 }
