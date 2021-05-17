12b64b4580e5749424550b23f29f69f17991b83b
Benjamin Domey <domeybenjamin1@gmail.com>
2021-05-17 14:47:19 +0000

    updated customer model design

--- internal/orm/models/customer.go
+++ internal/orm/models/customer.go
@@ -7,10 +7,12 @@ import (
 // Customer defines customers in the system. They could be businesses or individuals
 type Customer struct {
 	BaseModelSoftDelete
-	UserID string `gorm:"not null;"`
-	User   User
-	Type   *string // business, individual
-	TIN    *string
+	UserID   string `gorm:"not null;"`
+	User     User
+	LawyerID *string `gorm:"not null;"`
+	Lawyer   User
+	Type     *string // business, individual
+	TIN      *string
 
 	// For address
 	DigitalAddress    *string
