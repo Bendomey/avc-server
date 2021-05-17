0152d6683f9bc32302e47b7f8575079d4cf9e860
Benjamin Domey <domeybenjamin1@gmail.com>
2021-05-17 14:48:17 +0000

    package service updated

--- internal/orm/models/packageService.go
+++ internal/orm/models/packageService.go
@@ -9,13 +9,15 @@ const (
 
 type PackageService struct {
 	BaseModelSoftDelete
-	ServiceID   string `gorm:"not null;"`
-	Service     Service
-	PackageID   string `gorm:"not null;"`
-	Package     Package
-	Type        PackageServiceType
-	Quantity    *int
-	IsActive    *bool
-	CreatedByID string
-	CreatedBy   Admin
+	ServiceID     string `gorm:"not null;"`
+	Service       Service
+	PackageID     string `gorm:"not null;"`
+	Package       Package
+	Type          PackageServiceType
+	Quantity      *int
+	IsActive      *bool
+	CreatedByID   *string
+	CreatedBy     Admin
+	RequestedByID *string
+	RequestedBy   User
 }
