63bfb6a507781397ce9c0b636a2bde1b2171eb0b
Benjamin Domey <domeybenjamin1@gmail.com>
2021-05-17 14:48:24 +0000

    payment model updated

--- internal/orm/models/payment.go
+++ internal/orm/models/payment.go
@@ -1,5 +1,7 @@
 package models
 
+import "github.com/gofrs/uuid"
+
 type PaymentStatus string
 
 const (
@@ -11,9 +13,9 @@ const (
 // Payment defines payments a user creates
 type Payment struct {
 	BaseModelSoftDelete
-	Code             string  `gorm:"not null;"`
-	Amount           float64 `gorm:"not null;"`
-	ServicingID      *string `gorm:"not null;"`
+	Code             uuid.UUID `gorm:"primary_key;type:uuid;default:uuid_generate_v4()"`
+	Amount           float64   `gorm:"not null;"`
+	ServicingID      *string   `gorm:"not null;"`
 	Servicing        *Servicing
 	SubscriptionID   *string `gorm:"not null;"`
 	Subscription     *Subscription
